a618468a0a4882b1172ec888a0c55dbb73ad38cd
Crios_Chan <thomas.notolruob@gmail.com>
2019-11-15 20:26:20 +0100

    Update

--- src/main/java/fr/mcopvp/mcopvp/init/blockMod.java
+++ src/main/java/fr/mcopvp/mcopvp/init/blockMod.java
@@ -9,13 +9,15 @@ import net.minecraft.creativetab.CreativeTabs;
 
 public class blockMod {
 
-    public static Block jade_ore;
+    public static Block jade_ore, jade_block;
 
     public static void init(){
-        jade_ore = new blockBasique(Material.rock).setBlockName("jade_ore").setCreativeTab(CreativeTabs.tabBlock).setBlockName(References.MODID + ":jade_ore").setHardness(3).setResistance(20.0f);
+        jade_ore = new blockBasique(Material.rock).setBlockName("jade_ore").setCreativeTab(CreativeTabs.tabBlock).setBlockTextureName(References.MODID + ":jade_ore").setHardness(3).setResistance(20.0f);
+        jade_block = new blockBasique(Material.rock).setBlockName("jade_block").setCreativeTab(CreativeTabs.tabBlock).setBlockTextureName(References.MODID + ":jade_block").setHardness(3).setResistance(20.0f);
     }
 
     public static void register(){
         GameRegistry.registerBlock(jade_ore, jade_ore.getUnlocalizedName().substring(5));
+        GameRegistry.registerBlock(jade_block, jade_block.getUnlocalizedName().substring(5));
     }
 }
