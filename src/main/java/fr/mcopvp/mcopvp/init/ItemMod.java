48b93694ce19027370978e80e1eaa6acf4e58c33
fantome908 <52115036+fantome908@users.noreply.github.com>
2019-11-16 14:56:01 +0100

    ok

--- src/main/java/fr/mcopvp/mcopvp/init/ItemMod.java
+++ src/main/java/fr/mcopvp/mcopvp/init/ItemMod.java
@@ -8,16 +8,18 @@ import net.minecraft.item.Item;
 public class ItemMod {
 
 // je sais pas si sa fonction  XD
-    public static Item Jade, crystal;
+    public static Item Jade, crystal,etain ;
 
     public static void init(){
         Jade = new Item().setUnlocalizedName("jade").setCreativeTab(CreativeTabs.tabMaterials).setTextureName(References.MODID + ":jade");
         crystal = new Item().setUnlocalizedName("crystal").setCreativeTab(CreativeTabs.tabMaterials).setTextureName(References.MODID + ":crystal");
+        etain = new Item() .setUnlocalizedName( "etain" ).setCreativeTab(CreativeTabs.tabMaterials).setTextureName(References.MODID + ":etain")
     }
 
     public static void register(){
         GameRegistry.registerItem(Jade, "jade");
         GameRegistry.registerItem(crystal, "crystal");
+        GemeRegister.registerItem(etain, name:"etain");
     }
 
 }
